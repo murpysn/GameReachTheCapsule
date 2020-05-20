27b7f922d8b04d377319f2bef1fda3e7d4745d23
murpysn <nugiesatriya@gmail.com>
2020-05-20 17:14:32 +0700

    update

---
+++ Assets/Script/screen.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screen : MonoBehaviour
+{
+   	// Use this for initialization
+	void Start () {
+ 
+		Camera.main.aspect =  800f/480f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
